9486f8388dc2ce61328fc2cfced1beff1c6bce91
Peter Gillard-Moss <peter@gillardmoss.me.uk>
2014-03-18 13:01:46 +0000

    lein new app clj-http-s3

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-http-s3 "0.1.0-SNAPSHOT"
+  :description "Middleware to allow cli-http to authenticate with s3"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
