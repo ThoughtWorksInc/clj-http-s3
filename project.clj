58f4f89b646aa4853579fcdf60c7c33ad8adc848
Peter Gillard-Moss <peter@gillardmoss.me.uk>
2015-11-03 14:14:52 +0000

    clj-http is the latest version (2.0.0)

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.9.1"]
+                 [clj-http "2.0.0"]
                  [clj-time "0.6.0"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
